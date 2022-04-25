bbc3073374cd4c7c9b3a4843029db2470cf920fe
tadashi <tadashi.b@gmail.com>
2022-04-24 21:42:10 -0300

    Inserted comment at ListAllUsersController

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -9,6 +9,10 @@ class ListAllUsersController {
     const { user_id } = request.headers;
 
     try {
+      /** Como há a possibilidade de vir uma array de string de
+       * dentro do headers, então temos que garantir que vamos
+       * recuperar apenas uma valor do tipo string vindo do headers
+       */
       const userList = this.listAllUsersUseCase.execute({
         user_id: String(user_id),
       });
